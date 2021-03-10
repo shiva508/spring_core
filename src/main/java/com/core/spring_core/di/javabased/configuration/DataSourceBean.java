854183f1fb5f5fc7428559a32c99c3752f848130
shiva508 <dasarishiva1@gmail.com>
2021-03-10 09:51:26 +0530

    IOC

--- src/main/java/com/core/spring_core/di/javabased/configuration/DataSourceBean.java
+++ src/main/java/com/core/spring_core/di/javabased/configuration/DataSourceBean.java
@@ -1,7 +1,7 @@
 package com.core.spring_core.di.javabased.configuration;
 
 public class DataSourceBean {
-public void myDatabaseInstance() {
-System.out.println("Database Name:MySQL\n PORT:3600");
-}
+	public void myDatabaseInstance() {
+		System.out.println("Database Name:MySQL\n PORT:3600");
+	}
 }
