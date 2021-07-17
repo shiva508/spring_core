363c7b41b228f232e7df3c803bb186c16523e7ab
shiva508 <dasarishiva1@gmail.com>
2021-07-17 23:12:55 +0530

    Code

--- src/main/java/com/core/spring_core/listener/filtering/CustomerRegistrationEventFilteringListener.java
+++ src/main/java/com/core/spring_core/listener/filtering/CustomerRegistrationEventFilteringListener.java
@@ -8,7 +8,7 @@ import com.core.spring_core.listener.custom.CustomerRegistrationEvent;
 @Component
 public class CustomerRegistrationEventFilteringListener {
 	
-	@EventListener
+	@EventListener(condition = "#event.customerType=B2C")
 	public void handleRegistration(CustomerRegistrationEvent event){
 	    System.out.println("Dsaraararrara " + event.getName());
 	}
--- src/main/java/com/core/spring_core/listener/filtering/CustomerRegistrationPublisherFilterClient.java
+++ src/main/java/com/core/spring_core/listener/filtering/CustomerRegistrationPublisherFilterClient.java
@@ -3,6 +3,7 @@ package com.core.spring_core.listener.filtering;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import com.core.spring_core.listener.custom.CustomerRegistrationEvent;
@@ -11,16 +12,21 @@ import com.core.spring_core.listener.custom.CustomerRegistrationEventListener;
 //@Configuration
 public class CustomerRegistrationPublisherFilterClient {
 	@Autowired
-	private CustomerRegistrationEventFilteringListener customerRegistrationEventFilteringListener;
+	 CustomerRegistrationEventFilteringListener filteringListener;
 	@Autowired
-	private CustomerRegistrationPublisherFilterBean customerRegistrationPublisherFilterBean;
-	
-	
-	
+	 CustomerRegistrationPublisherFilterBean publisherFilterBean;
+	@Bean(name = "eventFilteringListener")
+	public CustomerRegistrationEventFilteringListener eventFilteringListener() {
+		return new CustomerRegistrationEventFilteringListener();
+	}
+	@Bean(name = "publisherFilterBean")
+	public CustomerRegistrationPublisherFilterBean publisherFilterBean() {
+		return new CustomerRegistrationPublisherFilterBean();
+	}
 	 
 	public static void main(String[] args) {
 		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(CustomerRegistrationPublisherFilterClient.class);
-		context.getBean(CustomerRegistrationEventFilteringListener.class).handleRegistration(new CustomerRegistrationEvent("Hellow Prends Who are yoo"));
+		((CustomerRegistrationEventFilteringListener) context.getBean("eventFilteringListener")).handleRegistration(new CustomerRegistrationEvent("Hellow Prends Who are yoo"));
 	}
 	
 }
